d92ab78344a65c28a917e954df6cd965da4c2182
José Ángel <joseangelmt@outlook.com>
2021-04-09 09:19:28 +0200

    Version 21.0.0.0

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,3 +1,3 @@
 @echo off
 nuget.exe pack Digi21.DigiNG.IO.Geomedia.nuspec
-nuget.exe sign Digi21.DigiNG.IO.Geomedia.17.2.0.nupkg -CertificatePath dwo.pfx -TimeStamper http://tsa.starfieldtech.com
+nuget.exe sign Digi21.DigiNG.IO.Geomedia.21.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://tsa.starfieldtech.com
