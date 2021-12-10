3fcd9650decd562c7548d772118ebf01855763a1
José Ángel <joseangelmt@outlook.com>
2021-12-10 13:36:49 +0100

    Version 22.1.0.0

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,3 +1,3 @@
 @echo off
 nuget.exe pack Digi21.DigiNG.IO.Geomedia.nuspec
-nuget.exe sign Digi21.DigiNG.IO.Geomedia.22.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
+nuget.exe sign Digi21.DigiNG.IO.Geomedia.22.1.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
